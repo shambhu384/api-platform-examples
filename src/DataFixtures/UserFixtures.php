fe2e1ee6cb4abd0cdaef573c7ba4a4efb57d2ef5
Shambhu Kumar <shambhu.kumar@fridaymediagroup.com>
2018-11-27 18:37:08 +0530

    JWT Authentication Implemented

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public const USER_REFERENCE = 'admin-user';
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('demo@demo.org');
+
+        $password = $this->encoder->encodePassword($user, 'pass_1234');
+        $user->setPassword($password);
+        $manager->persist($user);
+
+        $manager->flush();
+        // other fixtures can get this object using the UserFixtures::ADMIN_USER_REFERENCE constant
+    }
+}
